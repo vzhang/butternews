7b7278bbc5467c55873f27c5ee0b8475069d54a8
Aaron Zhang <mzhang8741@gmail.com>
2021-09-25 22:46:57 +0800

    add README

---
+++ ButterNews/API/Everything/BNEverythingRequest.h
@@ -0,0 +1,20 @@
+//
+//  BNEverythingRequest.h
+//  ButterNews
+//
+//  Created by Ming Zhang on 2021/9/25.
+//
+
+#import "BNRequest.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNEverythingRequest : BNRequest
+
+@property (nonatomic, copy) NSString *q;
+
+@property (nonatomic, copy) NSString *qInTitle;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ButterNews/API/Everything/BNEverythingRequest.m
@@ -0,0 +1,12 @@
+//
+//  BNEverythingRequest.m
+//  ButterNews
+//
+//  Created by Ming Zhang on 2021/9/25.
+//
+
+#import "BNEverythingRequest.h"
+
+@implementation BNEverythingRequest
+
+@end
